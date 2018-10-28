ed438b6b068bc949a2de0edb47d6568292740569
Duyen Ho <duyenho@gmail.com>
2018-10-28 11:15:04 +1100

    add graphql query for site title on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>About</h1>
+    <h1>About {data.site.siteMetadata.title} </h1>
     <p>Lorem ipsum dolor</p>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
